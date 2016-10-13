cb6304623c4da44f8a15864e1a4e2397123cb400
jiesoul <jiesoul@gmail.com>
2016-10-13 17:49:29 +0800

    review todo01

---
+++ realworldhaskell/ch12/Barcode.hs
@@ -0,0 +1,21 @@
+import Data.Array (Array(..), (!), bounds, elems, indices, ixmap, listArray)
+import Control.Applicative ((<$>))
+import Control.Monad (forM_)
+import Data.Char (digitToInt)
+import Data.Ix (Ix(..))
+import Data.List (fold', group, sort, sortBy, tails)
+import Data.Maybe (catMaybes, listToMaybe)
+import Data.Ratio (Ratio)
+import Data.Word (Word8)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Lazy.Char8 as L
+import qualified Data.Map as M
+
+import Parse
+
+checkDigit :: (Integral a) => [a] -> a
+checkDigit ds = 10 - (sum products `mod` 10)
+     where product = mapEveryOther (*3) (reverse ds)
+
+mapEveryOther :: (a -> a) -> [a] -> [a]
+mapEveryOther f = zipWith ($) (cycle [f,id])
---
+++ realworldhaskell/readme.md
@@ -0,0 +1,8 @@
+# note
+
+## ch01 start
+
+* ghc 编译器
+* ghci 解释器
+* runghc 运行程序
+
